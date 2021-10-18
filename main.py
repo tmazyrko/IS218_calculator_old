c6c4df53d58c60781b15070cdf0109490892b289
tmazyrko <tmazyrko@gmail.com>
2021-10-18 15:21:08 -0400

    Add some code for test

---
+++ main.py
@@ -0,0 +1,8 @@
+""" Content of calculator.py """
+
+def inc(x_value):
+    """ Increment function adds one to the x_value """
+    return x_value + 1
+def test_answer():
+    """ This tests the function """
+    assert inc(4) == 5
